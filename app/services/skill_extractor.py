897fbb77bc906af4ff2a3ac4b264d25033cbc6ec
abdul-manan-maqsood <helloabdulmanan@gmail.com>
2026-04-06 23:02:24 -0700

    Create basic skill database

---
+++ app/services/skill_extractor.py
@@ -0,0 +1,15 @@
+import spacy
+
+# Load spaCy model
+nlp = spacy.load("en_core_web_sm")
+
+SKILL_DB = [
+"python", "java", "c++", "javascript",
+    "fastapi", "django", "flask",
+    "machine learning", "deep learning",
+    "nlp", "data science",
+    "docker", "kubernetes",
+    "sql", "postgresql", "mongodb",
+    "git", "github",
+    "aws", "azure", "gcp"
+]
