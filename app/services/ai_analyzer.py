846bd3052e2a1bb82e5b3f9a3cb89996eb341351
abdul-manan-maqsood <helloabdulmanan@gmail.com>
2026-04-07 13:40:19 -0700

    Update LLM response to structured JSON format

--- app/services/ai_analyzer.py
+++ app/services/ai_analyzer.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -10,18 +11,27 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url="https://api.groq.
 
 def analyze_resume(text: str) -> str:
     """
-    Analyze resume text using AI and return feedback.
+    Analyze resume using LLM and return structured JSON output.
     """
 
     # Create prompt
     prompt = f"""
     You are an expert technical recruiter.
     
-    Analyze the following resume and provide:
-    1. Key strengths
-    2. Weakness
-    3. Missing skills
-    4. Specific improvement suggestions
+    Analyze the following resume and return ONLY valid JSON in this format:
+    
+    {{
+        "score": (0-100),
+        "strengths: [],
+        "weakness": [],
+        "missing_skills: [],
+        "suggestions": []
+    }}
+    
+    Rules:
+    - No explanation outside JSON
+    - Be concise and specific
+    - Score should reflect real-world hiring standards
     
     Resume:
     {text[:3000]}
@@ -31,11 +41,17 @@ def analyze_resume(text: str) -> str:
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[
-            {"role": "system", "content": "You are a professional resume reviewer."}, # Set AI behavior
+            {"role": "system", "content": "You return only JSON."}, # Set AI behavior
             {"role": "user", "content": prompt} # Send actual prompt
         ],
         temperature=0.5
     )
 
-    # Return AI-generated response text
-    return response.choices[0].message.content
+    # Store AI-generated response text
+    content = response.choices[0].message.content
+
+    # Convert text into JSON format safely
+    try:
+        return json.loads(content)
+    except json.JSONDecodeError:
+        return {"error": "Something went wrong. Please try again."}
