75f88870707d7975c1711fdc71c500281b19d074
abdul-manan-maqsood <helloabdulmanan@gmail.com>
2026-04-09 11:47:14 -0700

    Add file input handler to capture selected PDF

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 function App() {
   const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  }
   
   return (
     <div>
       <h1>ResuMentor</h1>
-      <input type="file" />
+      <input type="file" accept='.pdf' onChange={handleFileChange}/>
     </div>
   )
 }
