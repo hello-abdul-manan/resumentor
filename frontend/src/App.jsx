a5fcc4927528e8ba9741fbf44fda1e446abae923
abdul-manan-maqsood <helloabdulmanan@gmail.com>
2026-04-10 13:03:31 -0700

    Send selected file to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
   const [file, setFile] = useState(null);
@@ -6,21 +7,43 @@ function App() {
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   }
-  
-  const handleUpload = () => {
+
+  const handleUpload = async () => {
     if (!file) {
       alert("Please upload a PDF");
       return;
     }
 
-    console.log("File ready to upload:", file)
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/resume/upload-resume",
+        formData
+      );
+
+      console.log("Response:", response.data);
+
+    } catch (error) {
+      console.error("Upload error:", error);
+      alert("Error uploading resume");
+    }
   }
 
   return (
     <div>
       <h1>ResuMentor</h1>
-      <input type="file" accept='.pdf' onChange={handleFileChange}/>
-      <button onClick={handleUpload}>Upload & Analyze</button>
+
+      <input 
+        type="file" 
+        accept='.pdf' 
+        onChange={handleFileChange}
+      />
+
+      <button onClick={handleUpload}>
+        Upload & Analyze
+      </button>
     </div>
   )
 }
