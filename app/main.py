30facca3198169901a21e08f90aab49506eeb585
abdul-manan-maqsood <helloabdulmanan@gmail.com>
2026-04-10 10:42:44 -0700

    Configure CORS middleware to allow frontend to backend communication

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.resume_routes import router as resume_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Resumind API")
+
+# Add CORS middleware to allow frontend to backend commuincation
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include resume routes
 app.include_router(resume_router, prefix="/resume", tags=["Resume"])
